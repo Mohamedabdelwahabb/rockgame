109038f7fad11b5f9e2dfa8e09dfc8e907d262c2
Mohamedabdelwahabb <67339965+Mohamedabdelwahabb@users.noreply.github.com>
2023-05-11 23:44:25 +0200

    final update

--- script.js
+++ script.js
@@ -1,50 +1,91 @@
+const choices = ["rock", "paper", "scissors"];
+let playerScore = 0;
+let computerScore = 0;
+let exitGame = false;
+
 const computerPlayer = () => {
-  const choices = ["Rock", "Paper", "Scissors"];
   return choices[Math.floor(Math.random() * choices.length)];
 };
 
-let playerScore = 0;
-let computerScore = 0;
+const welcome = () => {
+  const confirmMessage = confirm(
+    "Rock Paper Scissors game! To start, press OK"
+  );
 
-const roundStart = (playerSelection, computerSelection) => {
-  playerSelection = playerSelection.toLowerCase();
-  if (playerSelection === computerSelection.toLowerCase()) {
-    return "No winner try again";
-  } else if (
-    (playerSelection === "rock" && computerSelection === "Scissors") ||
-    (playerSelection === "paper" && computerSelection === "Rock") ||
-    (playerSelection === "scissors" && computerSelection === "Paper")
-  ) {
-    playerScore++;
-    return `you win : ${playerSelection} beats ${computerSelection}`;
+  if (!confirmMessage) {
+    alert("Goodbye!");
   } else {
-    computerScore++;
-    return `you lose : ${computerSelection} beats ${playerSelection}`;
+    alert(
+      "Welcome! You need to select Rock, Paper, or Scissors to start. Press OK to continue."
+    );
+    playGame();
   }
 };
 
-function game() {
-  for (let i = 1; i <= 5; i++) {
-    const playerSelection = prompt(
-      `Round ${i}: Choose Rock, Paper, or Scissors`
-    );
-    const computerSelection = computerPlayer();
-    const result = roundStart(playerSelection, computerSelection);
-    console.log(result);
+const roundStart = (playerSelection, computerSelection) => {
+  if (playerSelection === computerSelection) {
+    return "No winner. Try again.";
+  } else if (
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "paper" && computerSelection === "rock") ||
+    (playerSelection === "scissors" && computerSelection === "paper")
+  ) {
+    playerScore++;
+    return `You win! ${playerSelection} beats ${computerSelection}.`;
+  } else {
+    computerScore++;
+    return `You lose! ${computerSelection} beats ${playerSelection}.`;
   }
+};
 
-  if (playerScore > computerScore) {
+const inputHandler = () => {
+  const errorAlert = "Invalid choice. Please choose rock, paper, or scissors.";
+  for (let i = 1; i <= 5; i++) {
+    let playerSelection = prompt(`Round ${i}: Choose Rock, Paper, or Scissors`);
+    if (playerSelection == null) {
+      const exitChoise = confirm("Are you sure you want to exit the game?");
+      if (exitChoise) {
+        alert("Goodbye!");
+        exitGame = true;
+        break;
+      } else {
+        i--;
+      }
+    } else if (playerSelection === "") {
+      alert(errorAlert);
+      i--;
+    } else if (!choices.includes(playerSelection.toLowerCase().trim())) {
+      alert(errorAlert);
+      i--;
+    } else {
+      const computerSelection = computerPlayer();
+      const result = roundStart(playerSelection, computerSelection);
+      console.log(result);
+    }
+  }
+};
+
+const finalScore = () => {
+  if (exitGame) {
+    return;
+  } else if (playerScore > computerScore) {
     console.log(
-      `You won the game! Final score: Your ${playerScore} : computer ${computerScore}`
+      `You won the game! Final score: Your ${playerScore} : Computer ${computerScore}`
     );
   } else if (playerScore < computerScore) {
     console.log(
-      `You lost the game! Final score: Your ${playerScore} : computer ${computerScore}`
+      `You lost the game! Final score: Your ${playerScore} : Computer ${computerScore}`
     );
   } else {
     console.log(
-      `It's a tie! Final score: Your ${playerScore} : computer ${computerScore}`
+      `It's a tie! Final score: Your ${playerScore} : Computer ${computerScore}`
     );
   }
-}
-game();
+};
+
+const playGame = () => {
+  inputHandler();
+  finalScore();
+};
+
+welcome();
